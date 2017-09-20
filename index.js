663a14042d0ab05f9960f66a9e3569823edc9151
github username <github email address>
2017-09-20 10:50:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+((name1,name2)=>{
+  let result = []
+  let nameArray = [name1, name2]
+  for (let i = 0;i<2; i++){
+  result.push(nameArray[(Math.floor(Math.random()*nameArray.length))])
+}
+debugger
+counter1 = 0
+counter2 = 0
+for (let i = 0;i<result.length;i++){
+  (result[i] == name1) ? (counter1++) : (counter2++)
+}
+(counter1 > counter2) ? ( `${name1}:${counter1}`) : ( `${name2}:${counter2}`)
+})()
